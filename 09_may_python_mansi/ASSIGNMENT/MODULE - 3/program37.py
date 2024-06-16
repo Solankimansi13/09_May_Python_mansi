02687c504cc0183b36043bbbeb8bb24f2cc84a3f
Solankimansi13 <mansisolanki1310@gmail.com>
2024-06-16 23:54:23 +0530

    your commit message

---
+++ 09_may_python_mansi/ASSIGNMENT/MODULE - 3/program37.py
@@ -0,0 +1,13 @@
+# Write a Python script to print a dictionary where the keys are numbers between 1 and 15. 
+
+
+
+dict = {}
+
+for i in range(1, 16):
+
+    square = i ** 2
+
+    dict[i] = square
+
+print(dict)
